c3417b33bdfb46a25fccbca31c505156c4dac6f0
BiscuitCuit15 <ldescombes@edenschool.fr>
2024-10-14 14:14:49 +0200

    feat (app) : first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//Exo1
+
+// function convertirCelsiusEnFahrenheit(celcius) {
+//     let f;
+//     f = celcius * 9/5 + 32;
+//     return f;
+// }
+// console.log(convertirCelsiusEnFahrenheit(0));
+
+//Exo2
+
+// function sommeTroisNombres(a, b, c) {
+//     let somme;
+//     somme = a + b + c;
+//     return somme;   
+// }
+// console.log(sommeTroisNombres(5, 10, 15));
+
+//Exo3
+
+// function heuresEnSecondes(heures) {
+//     let sec;
+//     sec = heures*3600;
+//     return sec;
+// }
+// console.log(heuresEnSecondes(2)); 
