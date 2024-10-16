7b36833973665f59adb9f7b15d1fd670ba29092c
BiscuitCuit15 <ldescombes@edenschool.fr>
2024-10-16 09:12:36 +0200

    feat (app) : update exo4 and exo5

--- main.js
+++ main.js
@@ -24,3 +24,32 @@
 //     return sec;
 // }
 // console.log(heuresEnSecondes(2)); 
+
+
+//Exo4 
+
+    // function peutVoter(age, nationalite) {
+    //     if (age >= 18 && nationalite === "française") {
+    //         return "Eligible";
+    //     } else {
+    //         return "Non éligible";
+    //     }
+    // }
+    // console.log(peutVoter(20, "française")); 
+    // console.log(peutVoter(16, "française")); 
+    // console.log(peutVoter(20, "espagnole"));
+
+//Exo5
+
+// function tarif(age) {
+//     if (age >= 5 && age <= 18) {
+//         return "Réduit"
+//     } else if (age < 5) {
+//         return "Gratuit"
+//     } else {
+//         return "Plein tarif"
+//     }
+//   }
+//   console.log(tarif(3)); 
+//   console.log(tarif(15)); 
+//   console.log(tarif(25));
